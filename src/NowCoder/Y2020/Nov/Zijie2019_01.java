143c468ceccf60198c4ac0bc9fe17cc1c5a8f2f3
mouchangzhi <waitingtbc@126.com>
2020-11-02 12:03:18 +0800

    20201017

---
+++ src/NowCoder/Y2020/Nov/Zijie2019_01.java
@@ -0,0 +1,25 @@
+package NowCoder.Y2020.Nov;
+
+import sun.security.util.Length;
+
+import java.util.Scanner;
+
+public class Zijie2019_01 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int line=sc.nextInt();
+        //String line2 = sc.next();
+//        System.out.println(line);
+        System.out.println("1111");
+        sc.nextLine();
+//        System.out.println(a+"222");
+//        String line="hellllloooo";
+//        System.out.println(line.replaceAll("(.)\\1+","$1$1").replaceAll("(.)\\1(.)\\2","$1$1$2"));
+        for (int i = 0; i < line; i++) {
+//            System.out.println(line);
+            System.out.println(sc.nextLine().replaceAll("(.)\\1+","$1$1").replaceAll("(.)\\1(.)\\2","$1$1$2"));
+        }
+//        System.out.println(sc.nextLine().replaceAll("(.)\\1+","$1$1").replaceAll("(.)\\1(.)\\2","$1$1$2"));
+
+    }
+}
